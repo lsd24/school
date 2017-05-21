cbc6b5b9fa4feb36f71d3648cf98351e37d085e6
lsd24 <714127018@qq.com>
2017-05-21 17:51:21 +0800

    521

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+header("content-type:text/html;charset=utf-8");
+define("APP_DEBUG",true);
+//指定项目目录
+define("APP_PATH","./school/");
+//指定项目名称
+define("APP_NAME","school");
+//引入ThinkPHP.php
+require("./thinkphp/ThinkPHP.php");
+
+ ?>
